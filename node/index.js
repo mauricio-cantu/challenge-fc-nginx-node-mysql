c76e5ad634ffd6b55600a642dee7730120e869f4
mauricio-cantu <mauriciocantujoc@gmail.com>
2024-12-24 01:24:55 -0300

    full cycle docker challenge

---
+++ node/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import mysql from 'mysql';
+
+const app = express();
+const port = 3000;
+
+const config = {
+  host: 'db',
+  user: 'root',
+  password: 'root',
+  database: 'nodedb'
+}
+
+const insertSql = `INSERT INTO people(name) VALUES ('New visitor');`;
+
+const selectSql = 'SELECT id, name FROM people;'
+
+app.get("/", (req, res) => {  
+  const conn = mysql.createConnection(config);
+
+  conn.query(insertSql);
+
+  conn.query(selectSql, (err, result) => {
+    if(err) throw err
+    res.send(`
+      <h1>Full cycle!!</h1>
+      <br />
+      <ul>
+        ${result.map(item => `<li>${item.name} (${item.id})</li>`)}
+      </ul>
+    `);
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Rodando na porta ${port}`);
+});
---
+++ readme.md
@@ -0,0 +1,17 @@
+# NGINX Reverse Proxy Challenge
+
+In this challenge, you'll set up NGINX as a reverse proxy for a Node.js application that adds records to a MySQL database.
+
+## Challenge Description
+
+1. **Access NGINX**: When accessing NGINX, it will forward the request to the Node.js application.
+2. **Node.js Application**: The application will add a record to the `people` table in the MySQL database.
+3. **Response**: The application will return a message containing "Full Cycle Rocks!" and the list of names in the database.
+
+## How to Run
+
+To run the containers, use the following command:
+
+```sh
+docker-compose up -d
+```
