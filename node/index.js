224150fb99522a18bcae0f0b58a25b7f773919c6
mauricio-cantu <mauriciocantujoc@gmail.com>
2024-12-27 00:59:02 -0300

    wrap up challenge

--- node/index.js
+++ node/index.js
@@ -1,32 +1,32 @@
 import express from "express";
-import mysql from 'mysql';
+import mysql from "mysql";
 
 const app = express();
 const port = 3000;
 
 const config = {
-  host: 'db',
-  user: 'root',
-  password: 'root',
-  database: 'nodedb'
-}
+  host: "db",
+  user: "root",
+  password: "root",
+  database: "nodedb",
+};
 
 const insertSql = `INSERT INTO people(name) VALUES ('New visitor');`;
 
-const selectSql = 'SELECT id, name FROM people;'
+const selectSql = "SELECT id, name FROM people;";
 
-app.get("/", (req, res) => {  
+app.get("/", (req, res) => {
   const conn = mysql.createConnection(config);
 
   conn.query(insertSql);
 
   conn.query(selectSql, (err, result) => {
-    if(err) throw err
+    if (err) throw err;
     res.send(`
       <h1>Full cycle!!</h1>
       <br />
       <ul>
-        ${result.map(item => `<li>${item.name} (${item.id})</li>`)}
+        ${result.map((item) => `<li>${item.name} (${item.id})</li>`)}
       </ul>
     `);
   });
